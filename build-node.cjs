bc06a252ed3a0b3b020dc23bbb7ece369bef7c88
ayecue <soerenwehmeier@googlemail.com>
2024-07-21 01:01:35 +0200

    0.5.13 change to esbuild

---
+++ build-node.cjs
@@ -0,0 +1,29 @@
+const esbuild = require('esbuild');
+
+const build = async () => {
+  try {
+    await esbuild
+      .build({
+        entryPoints: ['./out/extension.js'],
+        bundle: true,
+        outfile: 'extension.js',
+        globalName: 'miniscript',
+        sourcemap: false,
+        minify: true,
+        minifyWhitespace: true,
+        minifyIdentifiers: true,
+        minifySyntax: true,
+        target: 'ESNext',
+        platform: 'node',
+        treeShaking: true,
+        external: [
+          'vscode'
+        ]
+      });
+  } catch (err) {
+    console.error('Failed building project', { err });
+    process.exit(1);
+  }
+};
+
+build();
