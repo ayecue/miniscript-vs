2b98f4f84c726bb7c3ec26b05c37a7b150c33c1e
ayecue <soerenwehmeier@googlemail.com>
2024-08-15 00:19:46 +0200

    0.6.4 add client to activation

--- src/extension-browser.ts
+++ src/extension-browser.ts
@@ -1,44 +1,18 @@
 import vscode, { ExtensionContext } from 'vscode';
 
-import { activate as activateAutocomplete } from './autocomplete';
-import { activate as activateSignature } from './signature';
-import { activate as activateDefinition } from './definition';
-import { activate as activateDiagnostic } from './diagnostic';
-import { activate as activateHover } from './hover';
 import { activate as activateNextError } from './next-error';
 import { activate as activateRefresh } from './refresh';
 import { activate as activateSubscriptions } from './subscriptions';
-import { activate as activateSymbol } from './symbol';
 import { activate as activateTransform } from './transform';
-import { activate as activateFormatter } from './formatter';
+import { activate as activateLanguageClient } from './language-client-web';
 
 export function activate(context: ExtensionContext) {
-  const config = vscode.workspace.getConfiguration('miniscript');
-
   activateRefresh(context);
   activateSubscriptions(context);
 
-  if (config.get<boolean>('hoverdocs')) {
-    activateHover(context);
-  }
-
-  if (config.get<boolean>('autocomplete')) {
-    activateAutocomplete(context);
-    activateSignature(context);
-  }
-
   activateTransform(context);
   activateNextError(context);
-  activateDefinition(context);
-  activateSymbol(context);
-
-  if (config.get<boolean>('formatter')) {
-    activateFormatter(context);
-  }
-
-  if (config.get<boolean>('diagnostic')) {
-    activateDiagnostic(context);
-  }
+  activateLanguageClient(context);
 }
 
 export function deactivate() { }
