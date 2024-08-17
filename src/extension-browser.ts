160078f73bd36175f0c1b7937e04fa8aa6cf5e5c
ayecue <soerenwehmeier@googlemail.com>
2024-08-17 23:18:24 +0200

    0.6.4 remove obselete files

--- src/extension-browser.ts
+++ src/extension-browser.ts
@@ -1,15 +1,10 @@
 import vscode, { ExtensionContext } from 'vscode';
 
 import { activate as activateNextError } from './next-error';
-import { activate as activateRefresh } from './refresh';
-import { activate as activateSubscriptions } from './subscriptions';
 import { activate as activateTransform } from './transform';
 import { activate as activateLanguageClient } from './language-client-web';
 
 export function activate(context: ExtensionContext) {
-  activateRefresh(context);
-  activateSubscriptions(context);
-
   activateTransform(context);
   activateNextError(context);
   activateLanguageClient(context);
--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,10 @@ import { ExtensionContext } from 'vscode';
 import { activate as activateBuild } from './build';
 import { activate as activateDebug } from './debug';
 import { activate as activateNextError } from './next-error';
-import { activate as activateRefresh } from './refresh';
-import { activate as activateSubscriptions } from './subscriptions';
 import { activate as activateTransform } from './transform';
 import { activate as activateLanguageClient } from './language-client';
 
 export function activate(context: ExtensionContext) {
-  activateRefresh(context);
-  activateSubscriptions(context);
-
   activateDebug(context);
   activateBuild(context);
   activateTransform(context);
